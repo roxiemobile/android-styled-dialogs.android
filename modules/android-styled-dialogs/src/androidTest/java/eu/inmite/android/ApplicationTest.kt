0b0759569da0bc8989ade662a3e93bdb7ddc1704
Alexander Bragin <alexander.bragin@gmail.com>
2017-07-18 01:02:24 +0300

    Added Kotlin support.

---
+++ modules/android-styled-dialogs/src/androidTest/java/eu/inmite/android/ApplicationTest.kt
@@ -0,0 +1,6 @@
+package eu.inmite.android
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
---
+++ modules/examples/src/androidTest/java/com/roxiemobile/examples/androidstyleddialogs/ApplicationTest.kt
@@ -0,0 +1,6 @@
+package com.roxiemobile.examples.androidstyleddialogs
+
+import android.app.Application
+import android.test.ApplicationTestCase
+
+class ApplicationTest : ApplicationTestCase<Application>(Application::class.java)
